5a3998e6e189349cbfb27005c8cbc6b470212087
moaaobw <106493217+moaaobw@users.noreply.github.com>
2022-06-14 12:42:22 +0300

    Add files via upload

---
+++ handlers/play.py
@@ -0,0 +1,247 @@
+import os
+from os import path
+from pyrogram import Client, filters
+from pyrogram.types import Message, Voice, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import UserAlreadyParticipant
+from callsmusic import callsmusic, queues
+from callsmusic.callsmusic import client as USER
+from helpers.admins import get_administrators
+import requests
+import aiohttp
+from youtube_search import YoutubeSearch
+import converter
+from downloaders import youtube
+from config import DURATION_LIMIT, SUPPORT_GROUP
+from helpers.filters import command
+from helpers.decorators import errors
+from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
+import aiofiles
+import ffmpeg
+from pytgcalls import StreamType
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputStream
+
+
+def transcode(filename):
+    ffmpeg.input(filename).output("input.raw", format='s16le', acodec='pcm_s16le', ac=2, ar='48k').overwrite_output().run()
+    os.remove(filename)
+
+# Convert seconds to mm:ss
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+@Client.on_message(
+    command(["play", "شغل", "تشغيل"])
+ async def play(c: Client, m: Message):
+    await m.delete()
+    do = requests.get(f"https://api.telegram.org/bot5214195753:AAEOzHC6pNXa3G_fxP-MicuJL4nyuUzPq2E/getChatMember?chat_id=@S_T_Dl&user_id={m.from_user.id}").text
+    if do.count("left") or do.count("Bad Request: user not found"):
+        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني \n— — — — — — — — — — — — —\n - @S_T_Dl ↫ ")
+      & filters.group
+    & ~filters.edited
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def play(_, message: Message):
+    global que
+    global useer
+
+    await message.delete()
+
+    fallen = await message.reply("» ابشر يتم البحث 🔎")
+
+    chumtiya = message.from_user.mention
+
+    administrators = await get_administrators(message.chat)
+    chid = message.chat.id
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "Anonymous"
+    usar = user
+    wew = usar.id
+    try:
+        await _.get_chat_member(chid, wew)
+    except:
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                try:
+                    invitelink = await _.export_chat_invite_link(chid)
+                except:
+                    await fallen.edit(
+                        "<b>» ꜰɪʀsᴛʟʏ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ʙᴀʙʏ</b>")
+                    return
+
+                try:
+                    await USER.join_chat(invitelink)
+                    await USER.send_message(
+                        message.chat.id, "» ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇssꜰᴜʟʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ ʙᴀʙʏ, ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴘʟᴀʏ sᴏɴɢs​.")
+
+                except UserAlreadyParticipant:
+                    pass
+                except Exception:
+                    await fallen.edit(
+                        f"<b>» ᴀssɪsᴛᴀɴᴛ ɪs ɴᴏᴛ ɪɴ ᴛʜɪs ᴄʜᴀᴛ ʙᴀʙʏ, sᴇɴᴅ /join ғɪʀsᴛ ᴛɪᴍᴇ ᴛᴏ ᴏʀᴅᴇʀ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴛᴏ ᴊ​ᴏɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ.")
+    try:
+        await USER.get_chat(chid)
+    except Exception as e:
+        await fallen.edit(
+            f"<i>» ᴀssɪsᴛᴀɴᴛ ғᴀɪʟᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜɪs ᴄʜᴀᴛ.</i>\n\nʀᴇᴀsᴏɴ : {e}")
+        return
+
+    audio = (
+        (message.reply_to_message.audio or message.reply_to_message.voice)
+        if message.reply_to_message
+        else None
+    )
+    url = get_url(message)
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"» sᴏʀʀʏ ʙᴀʙʏ, ᴛʀᴀᴄᴋ ʟᴏɴɢᴇʀ ᴛʜᴀɴ  {DURATION_LIMIT} ᴍɪɴᴜᴛᴇs ᴀʀᴇ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴘʟᴀʏ"
+            )
+
+        file_name = get_file_name(audio)
+        title = file_name
+        duration = round(audio.duration / 60)
+        views = "Locally added"
+
+        file_path = await converter.convert(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name))
+            else file_name
+        )
+
+    elif url:
+        try:
+            results = YoutubeSearch(url, max_results=1).to_dict()
+            # print results
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            thumb_name = f"thumb{title}.jpg"
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, "wb").write(thumb.content)
+            duration = results[0]["duration"]
+            url_suffix = results[0]["url_suffix"]
+            views = results[0]["views"]
+            durl = url
+            durl = durl.replace("youtube", "youtubepp")
+
+            secmul, dur, dur_arr = 1, 0, duration.split(":")
+            for i in range(len(dur_arr) - 1, -1, -1):
+                dur += int(dur_arr[i]) * secmul
+                secmul *= 60
+
+        except Exception as e:
+            title = "NaN"
+            duration = "NaN"
+            views = "NaN"
+
+        if (dur / 60) > DURATION_LIMIT:
+            await fallen.edit(
+                f"» sᴏʀʀʏ ʙᴀʙʏ, ᴛʀᴀᴄᴋ ʟᴏɴɢᴇʀ ᴛʜᴀɴ  {DURATION_LIMIT} ᴍɪɴᴜᴛᴇs ᴀʀᴇ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴘʟᴀʏ"
+            )
+            return
+        file_path = await converter.convert(youtube.download(url))
+    else:
+        if len(message.command) < 2:
+            return await fallen.edit(
+                "» ياغالي اكتب شي للبحث او اعطيني اسم الاغنيه صالح 🤦🏻‍♂️"
+            )
+        await fallen.edit("🔎")
+        query = message.text.split(None, 1)[1]
+        # print(query)
+        try:
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            url = f"https://youtube.com{results[0]['url_suffix']}"
+            # print results
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            thumb_name = f"thumb{title}.jpg"
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, "wb").write(thumb.content)
+            duration = results[0]["duration"]
+            url_suffix = results[0]["url_suffix"]
+            views = results[0]["views"]
+            durl = url
+            durl = durl.replace("youtube", "youtubepp")
+
+            secmul, dur, dur_arr = 1, 0, duration.split(":")
+            for i in range(len(dur_arr) - 1, -1, -1):
+                dur += int(dur_arr[i]) * secmul
+                secmul *= 60
+
+        except Exception as e:
+            await fallen.edit(
+                "» عفوآ عيني عندك مشكلة بل اغنيه اكتبها صح يا ابوراس مربع 🙂"
+            )
+            print(str(e))
+            return
+
+        if (dur / 60) > DURATION_LIMIT:
+            await fallen.edit(
+                f"» sᴏʀʀʏ ʙᴀʙʏ, ᴛʀᴀᴄᴋ ʟᴏɴɢᴇʀ ᴛʜᴀɴ  {DURATION_LIMIT} ᴍɪɴᴜᴛᴇs ᴀʀᴇ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴘʟᴀʏ"
+            )
+            return
+        file_path = await converter.convert(youtube.download(url))
+    ACTV_CALLS = []
+    chat_id = message.chat.id
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) in ACTV_CALLS:
+        position = await queues.put(chat_id, file=file_path)
+        await message.reply_text(
+            text=f"**» ابشر تم التشغيل {position} **\n📌 **الاغنيه :**[{title[:65]}]({url})\n\n🕕** عدد :** `{duration}` **الدقائق**\n💕** طلب الحلو​ : **{chumtiya}",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("• كروب البوت •", url=f"https://t.me/{SUPPORT_GROUP}"),
+                    InlineKeyboardButton("» اخفاء القائمة «", callback_data="close_play")
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+    else:
+        await callsmusic.pytgcalls.join_group_call(
+                chat_id,
+                InputStream(
+                    InputAudioStream(
+                        file_path,
+                    ),
+                ),
+                stream_type=StreamType().local_stream,
+            )
+
+        await message.reply_text(
+            text=f"**» ابشر تم التشغيل «**\n📌 **الاغنيه:** [{title[:65]}]({url})\n🕕 **عدد:** `{duration}` الدقائق\n💕 **طلب الحلو​:** {chumtiya}\n💔 **الدردشة​:** `{message.chat.title}`\n🎥 **نوع التشغيل:** موسيقى\n",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("• كروب البوت •", url=f"https://t.me/{SUPPORT_GROUP}"),
+                    InlineKeyboardButton("» اخفاء القائمة «", callback_data="close_play")
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+
+    return await fallen.delete()
+
+@Client.on_callback_query(filters.regex("close_play"))
+async def in_close_play(_, query: CallbackQuery):
+    await query.message.delete()
---
+++ handlers/start.py
@@ -0,0 +1,50 @@
+import asyncio
+
+from helpers.filters import command
+from config import BOT_NAME as bn, BOT_USERNAME as bu, SUPPORT_GROUP, OWNER_USERNAME as me, START_IMG
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.delete()
+    await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
+    await message.reply_photo(
+        photo=f"{START_IMG}",
+        caption=f"""**━━━━━━━━━━━━━━━━━━
+💔 هلا عمري {message.from_user.mention()} !
+        انا بوت اسمي ماس استطيع تشغيل الاغاني في المكالمه المرئيه
+يمكنني التشغيل بصوت رائع وبدون اي مشاكل او تقطيع في الاغنيه
+ اضفني الى مجموعتك وامنحني الصلاحيات لكي اعمل بشكل صحيح
+┏━━━━━━━━━━━━━━┓
+┣★ نشكر كل مين اضاف
+┣★ [Source Cobra](t.me/{me})
+┣★ بوتات ســـورس ماس
+┗━━━━━━━━━━━━━━┛
+
+ لمعرفة اوامر هذا البوت اضغط على زر الاوامر ان واجهة مشكلة تواصل مع المطور [َٰـُـٰٓيَٰـُـٰٓبَٰـُـٰٓر🇧 🇪 🇵 🇵 🇪 🇷](t.me/{me})
+━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "اضفني الى مجموعتك", url=f"https://t.me/{bu}?startgroup=true"
+                       ),
+                  ],[
+                    InlineKeyboardButton(
+                        "مطور البوت", url=f"https://t.me/{me}"
+                    ),
+                    InlineKeyboardButton(
+                        "كروب الدعم", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        "الاوامر", url=f"https://telegra.ph/%F0%9D%99%B2%E1%B4%8F%E1%B4%8D%E1%B4%8D%E1%B4%80%C9%B4%E1%B4%85s-04-06"
+                    ),
+                    InlineKeyboardButton(
+                        "قناة البوت الرسمية", url="https://t.me/S_T_Dl"
+                    )]
+            ]
+       ),
+    )
